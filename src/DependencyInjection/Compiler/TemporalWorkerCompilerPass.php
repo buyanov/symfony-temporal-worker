ef291a67394ab1d5e9b064b4d7351f9377df4a91
d.buyanov <d.buyanov@tool-kit.tech>
2023-09-20 13:44:21 +0300

    WIP

--- src/DependencyInjection/Compiler/TemporalWorkerCompilerPass.php
+++ src/DependencyInjection/Compiler/TemporalWorkerCompilerPass.php
@@ -4,20 +4,14 @@ declare(strict_types=1);
 
 namespace Buyanov\SymfonyTemporalWorker\DependencyInjection\Compiler;
 
-use Buyanov\SymfonyTemporalWorker\Temporal\Worker\TemporalWorker;
-use Buyanov\SymfonyTemporalWorker\Temporal\Worker\TemporalWorkerInterface;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Reference;
 
 class TemporalWorkerCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
     {
-        $definition = $container
-            ->register(TemporalWorkerInterface::class, TemporalWorker::class)
-            ->addArgument(new Reference('service_container'))
-            ->setPublic(false);
+        $definition = $container->getDefinition('temporal.worker');
 
         $taggedActivities = $container->findTaggedServiceIds('temporal.activity');
 
